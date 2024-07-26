b6165d5a1c907395a076d1ee196a5bd9fe67d4f5
inoue-y01 <inoue-y01@asoalpha-net.com>
2024-07-26 13:12:31 +0900

    ssl対応

---
+++ docker/reverse-proxy/certificate/generate.sh
@@ -0,0 +1,3 @@
+openssl genrsa -out server.key
+openssl req -new -key server.key -out server.csr -subj "/CN=localhost"
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt -extfile extfile.txt
